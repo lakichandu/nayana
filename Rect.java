b77b49ad7b4f403be1cead5ea160d9858d223226
unknown <tushargijomarvick@gmail.com>
2023-02-13 17:51:41 +0530

    moving

---
+++ Rect.java
@@ -0,0 +1,18 @@
+class Rect
+{
+	static void area(int a,int b)
+	{
+		//int a=10;
+			//int b=20;
+		//double theta=30;
+			//int r=4;
+		double result=(a*b);
+		System.out.println(result);
+	}
+	public static void main(String[] args)
+	{
+		area(23,45);
+		area(67,89);
+	}
+
+}
