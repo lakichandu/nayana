05e29eb894df51c1ca1127f323a3a7d89b4052ca
unknown <tushargijomarvick@gmail.com>
2023-02-13 17:50:16 +0530

    moving

---
+++ squar.java
@@ -0,0 +1,18 @@
+class Squar
+{
+	static void area()
+	{
+		int a=10;
+		//	int h=20;
+		//double theta=30;
+			//int r=4;
+		double result=(a*a);
+		System.out.println(result);
+	}
+	public static void main(String[] args)
+	{
+		area();
+		
+	}
+
+}
