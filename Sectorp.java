4a29e2aafc33c924ecb8b13ba3fecbe1ba83be1e
unknown <tushargijomarvick@gmail.com>
2023-02-13 18:26:53 +0530

    moving

---
+++ Sectorp.java
@@ -0,0 +1,18 @@
+class Triangle
+{
+	static void area()
+	{
+		int a=10;
+			int b=20;
+		//double theta=30;
+			//int r=4;
+		double result=(a*b)/2.0;
+		System.out.println(result);
+	}
+	public static void main(String[] args)
+	{
+		area();
+		area();
+	}
+
+}
